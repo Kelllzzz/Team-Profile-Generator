817ff618a809d70d4db72aaedd61120fe49c71b0
Kelllzzz <kellakhigbe@gmail.com>
2024-02-27 11:50:12 +0000

    updated js files to include super and updated package.json file

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -3,9 +3,7 @@ const Employee = require("./Employee");
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 class Engineer extends Employee { 
     constructor(name, id, email, github) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
+        super(name, id, email); //inherits properties from the parent class
         this.github = github
     }
     getGithub() {
--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -3,10 +3,8 @@ const Employee = require("./Employee");
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 class Manager extends Employee {
     constructor(name, id, email, officeNumber) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.officeNumber = this.officeNumber;
+        super(name, id, email); //inherits properties from the parent class
+        this.officeNumber = officeNumber;
     }
     getOfficeNumber() {
         return this.officeNumber;

51def88ada9e5a175676b09597bca803c1466488
Kelllzzz <kellakhigbe@gmail.com>
2024-02-27 15:53:30 +0000

    added code to gather development team members instructions

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,56 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Name of Employee ? :',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Please Provide Employee id:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address ?:',
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is your role:',
+        choices: ['Manager', 'Engineer', 'Intern'],
+    },
+
+];
+
+//if Manager role is selected
+const questionsForManager = [
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Please input your office number:',
+    },
+];
+
+//if Engineer role is selected
+const questionsForEngineer = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username ? :',
+    },
+];
+
+ //if intern role is selected
+const questionsForIntern = [  
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What school do you attend ? :',
+    },
+];
+
